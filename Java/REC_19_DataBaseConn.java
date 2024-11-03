932eea8e55c4c3103bc10dce82b20818748e4052
ankith5980 <ankithpratheesh147@gmail.com>
2024-11-03 18:47:15 +0530

    Program Added

--- Java/REC_19_DataBaseConn.java
+++ Java/REC_19_DataBaseConn.java
@@ -1,3 +1,42 @@
 package Java;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class REC_19_DataBaseConn {
+
+
+
+		public static void main(String[] args) throws ClassNotFoundException, SQLException {
+			
+			// register the database
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			//step 2 - Establish the connection 
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","root");
+			System.out.println("connection created");
+			//step 3- create statement
+			Statement stmt=con.createStatement();
+			
+			//step 4- execute query
+			ResultSet rs= stmt.executeQuery("select *from students;");
+			
+			while(rs.next())
+			{
+				int id =rs.getInt("id");
+				String name=rs.getString("name");
+
+				
+				System.out.println("**************\n");
+				System.out.println("ID:\n"+id);
+				System.out.println("Name:\n"+name);
+
+			}
+			
+		}
+
+	}

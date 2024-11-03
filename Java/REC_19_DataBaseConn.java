f22c539e93cd3a92fe06d6f7a1f0ed931acaa5e5
ankith5980 <ankithpratheesh147@gmail.com>
2024-11-03 19:39:39 +0530

    Minor Changes

--- Java/REC_19_DataBaseConn.java
+++ Java/REC_19_DataBaseConn.java
@@ -1,6 +1,5 @@
 package Java;
 
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,34 +8,25 @@ import java.sql.Statement;
 
 public class REC_19_DataBaseConn {
 
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
 
+        Class.forName("com.mysql cj.jdbc Driver");
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb", "root", "root");
+        System.out.println("Connection Created !");
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery("Select * from employes");
+        while (rs.next()){
+            String id = rs.getString("id");
+            String name = rs.getString("name");
+            String design = rs.getString("design");
+            String dept = rs.getString("dept");
 
-		public static void main(String[] args) throws ClassNotFoundException, SQLException {
-			
-			// register the database
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			
-			//step 2 - Establish the connection 
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","root");
-			System.out.println("connection created");
-			//step 3- create statement
-			Statement stmt=con.createStatement();
-			
-			//step 4- execute query
-			ResultSet rs= stmt.executeQuery("select *from students;");
-			
-			while(rs.next())
-			{
-				int id =rs.getInt("id");
-				String name=rs.getString("name");
-
-				
-				System.out.println("**************\n");
-				System.out.println("ID:\n"+id);
-				System.out.println("Name:\n"+name);
-
-			}
-			
-		}
-
-	}
+            System.out.println("*  *  *  * \n");
+            System.out.println("ID :: " + id);
+            System.out.println("Name :: " + name);
+            System.out.println("Designation :: " + design);
+            System.out.println("Department :: " + dept);
+        }
+        
+    }
+}

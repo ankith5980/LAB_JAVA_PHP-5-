7ab1d940655083be539ec31d818db8081ed820ae
ankith5980 <ankithpratheesh147@gmail.com>
2024-11-03 22:14:23 +0530

    Minor Changes

--- Java/REC_15_Abstract.java
+++ Java/REC_15_Abstract.java
@@ -57,23 +57,31 @@ class Sphere extends ThreeD{
 public class REC_15_Abstract{
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
+
             System.out.println("Enter the side length of the Square :: ");
             double SquareSide = sc.nextDouble();
             Square sq = new Square(SquareSide);
             System.out.println("Area of the square :: " + sq.area());
+
+
             System.out.println(("Enter the base and height of the triangle :: "));
             double triangleBase = sc.nextDouble();
             double triangleHeight = sc.nextDouble();
             Triangle tri = new Triangle(triangleBase, triangleHeight);
             System.out.println("Area of the Triangle :: " + tri.area());
+
+
             System.out.println("Enter the side of the cube :: ");
             double cubeSide = sc.nextDouble();
             Cube cb = new Cube(cubeSide);
             System.out.println("Area of the cube :: " + cb.SurfaceArea());
+
+
             System.out.println("Enter the radius of the sphere :: ");
             double SphereRadius = sc.nextDouble();
             Sphere sp = new Sphere(SphereRadius);
             System.out.println("Area of the Sphere :: " + sp.SurfaceArea());
+            
         }
     }
 }

958b363e8cbc9d3c0984e94606bf8ad8c6dcbc6c
ankith5980 <ankithpratheesh147@gmail.com>
2024-11-06 11:57:48 +0530

    Updated

---
+++ PHP/php(A)/pgm19_StringReverse.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>String Reverse</title>
+</head>
+<body>
+    <center>
+        <h1><b><u>String Reversing</u></b></h1>
+
+        <form>
+            <table>
+                <tr>
+                    <td>
+                        <label for="Enter the string">Enter the string</label>
+                    </td>
+                    <td>
+                        <input type="text" placeholder="Enter the string here" name="strings" required>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <button type="submit">Submit</button>
+                    </td>
+                </tr>
+            </table>
+        </form>
+    </center>
+</body>
+</html>
+
+
+<!-- PHP Section -->

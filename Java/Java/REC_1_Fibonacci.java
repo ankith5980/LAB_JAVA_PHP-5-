50fe38e313509b169227c40161ef4b01cc721289
ankith5980 <ankithpratheesh147@gmail.com>
2025-03-12 16:00:49 +0530

    Updated

--- Java/Java/REC_1_Fibonacci.java
+++ Java/Java/REC_1_Fibonacci.java
@@ -7,23 +7,18 @@ public class REC_1_Fibonacci {
     public static void main(String[] args) {
         int fib1=0;
         int fib2=1;
-        int fib3, i;
+        int fib3;
 
         System.out.print("Enter the limit :: ");
         
         try (Scanner sc = new Scanner(System.in)) {
             int n = sc.nextInt();
 
-            System.out.println(fib1);
-            System.out.println(fib2);
-
-            for(i=0; i<=(n-3); i++){
-
+            while (fib1<n) {
+                System.out.println(fib1);
                 fib3=fib1+fib2;
                 fib1=fib2;
                 fib2=fib3;
-
-                System.out.println(fib3);
             }
         }
     }
